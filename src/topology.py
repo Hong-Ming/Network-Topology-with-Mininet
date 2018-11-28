93342aad5ed19d0e3fb978169cf7d022115e6614
Hong-Ming <h15731573@gmail.com>
2018-11-28 20:50:19 +0800

    topology.py version10

--- src/topology.py
+++ src/topology.py
@@ -29,22 +29,23 @@ class mytopo(Topo):
         host5 = self.addHost('h5')
         host6 = self.addHost('h6')
 
-        self.addLink(switch9, switch7, bw = 40, delay = '5ms', loss = 2)
-        self.addLink(switch8, switch9, bw = 50, delay = '4ms', loss = 3)
-        self.addLink(switch1, switch7, bw = 23, delay = '1ms', loss = 8)
-        self.addLink(switch2, switch7, bw = 18, delay = '2ms', loss = 9)
-        self.addLink(switch3, switch7, bw = 15, delay = '3ms', loss = 5)
+        self.addLink(switch9, switch7, bw = 40, delay = '5ms',  loss = 2)
+        self.addLink(switch9, switch7, bw = 50, delay = '4ms',  loss = 3)
 
-        self.addLink(switch4, switch8, bw = 19, delay = '80us', loss = 7)
-        self.addLink(switch5, switch8, bw = 30, delay = '95us', loss = 2)
-        self.addLink(switch6, switch8, bw = 20, delay = '60us', loss = 6)
+        self.addLink(switch7, switch1, bw = 23, delay = '1ms',  loss = 8)
+        self.addLink(switch7, switch2, bw = 18, delay = '2ms',  loss = 9)
+        self.addLink(switch7, switch3, bw = 15, delay = '3ms',  loss = 5)
 
-        self.addLink(host1, switch1, bw = 10, delay = '50us', loss = 12)
-        self.addLink(host2, switch2, bw = 5, delay = '2ms', loss = 3)
-        self.addLink(host3, switch3, bw = 7, delay = '63us', loss = 9)
-        self.addLink(host4, switch4, bw = 12, delay = '40us', loss = 14)
-        self.addLink(host5, switch5, bw = 15, delay = '30us', loss = 18)
-        self.addLink(host6, switch6, bw = 3, delay = '5ms', loss = 2)
+        self.addLink(switch8, switch4, bw = 19, delay = '80us', loss = 7)
+        self.addLink(switch8, switch5, bw = 30, delay = '95us', loss = 2)
+        self.addLink(switch8, switch6, bw = 20, delay = '60us', loss = 6)
+
+        self.addLink(switch1, host1,   bw = 10, delay = '50us', loss = 12)
+        self.addLink(switch2, host2,   bw = 5,  delay = '2ms',  loss = 3)
+        self.addLink(switch3, host3,   bw = 7,  delay = '63us', loss = 9)
+        self.addLink(switch4, host4,   bw = 12, delay = '40us', loss = 14)
+        self.addLink(switch5, host5,   bw = 15, delay = '30us', loss = 18)
+        self.addLink(switch6, host6,   bw = 3,  delay = '5ms',  loss = 2)
 
 topo = mytopo()
 net = Mininet(
